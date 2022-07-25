8007d0f03a1f66ae21e9cfac24f8829beccbb02e
Krishna Kumar <krishnakumar.211it034@nitk.edu.in>
2022-07-26 01:54:27 +0530

    Till lecture 8 there is no error

--- app/src/main/java/com/example/instantchattingapp/adapters/ChatAdapter.java
+++ app/src/main/java/com/example/instantchattingapp/adapters/ChatAdapter.java
@@ -1,43 +1,92 @@
 package com.example.instantchattingapp.adapters;
 
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.instantchattingapp.R;
+import com.example.instantchattingapp.databinding.RecievedMessageContainerBinding;
+import com.example.instantchattingapp.databinding.SentMessageContainerBinding;
+import com.example.instantchattingapp.models.ChatMessage;
 
-public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter .ChatHolder>{
+import java.util.ArrayList;
+
+public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private final Bitmap recieverProfileImage;
+    private final ArrayList<ChatMessage> chatMessages;
+    private final String SenderId;
+    private final int VIEW_TYPE_SENT = 1;
+    private final int VIEW_TYPE_RECIEVE = 2;
+
+    public ChatAdapter(Bitmap recieverProfileImage, ArrayList<ChatMessage> chatMessages, String senderId) {
+        this.recieverProfileImage = recieverProfileImage;
+        this.chatMessages = chatMessages;
+        SenderId = senderId;
+    }
+
     @NonNull
     @Override
-    public ChatAdapter.ChatHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if(viewType==VIEW_TYPE_SENT){
+            return new sentMessageViewHolder(SentMessageContainerBinding.inflate(
+                    LayoutInflater.from(parent.getContext()),
+                    parent,
+                    false));
+        } else {
+            return new recievedMessageViewHolder(RecievedMessageContainerBinding.inflate(
+                    LayoutInflater.from(parent.getContext()),
+                    parent,
+                    false));
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ChatAdapter.ChatHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if(getItemViewType(position)== VIEW_TYPE_SENT){
+            ((sentMessageViewHolder)holder).setData(chatMessages.get(position));
+        } else {
+            ((recievedMessageViewHolder)holder).setData(chatMessages.get(position),recieverProfileImage);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return chatMessages.size();
     }
 
-    class ChatHolder extends RecyclerView.ViewHolder{
-        TextView textMessage,textDateTime;
-        ImageView userProfile;
-        public ChatHolder(@NonNull View itemView) {
-            super(itemView);
-            textDateTime = itemView.findViewById(R.id.textDateTime);
-            textMessage = itemView.findViewById(R.id.textMessage);
-            userProfile = itemView.findViewById(R.id.image_profile);
+    @Override
+    public int getItemViewType(int position) {
+        if(chatMessages.get(position).getSenderId().equals(SenderId)){
+            return VIEW_TYPE_SENT;
+        } else {
+            return VIEW_TYPE_RECIEVE;
         }
     }
-    void setData(){
 
+    public static class sentMessageViewHolder extends RecyclerView.ViewHolder{
+        private SentMessageContainerBinding binding;
+        public sentMessageViewHolder(SentMessageContainerBinding binding) {
+            super(binding.getRoot());
+            this.binding=binding;
+        }
+        private void setData(ChatMessage chatMessage){
+            binding.textMessage.setText(chatMessage.getMessage());
+            binding.textDateTime.setText(chatMessage.getDateTime());
+        }
+    }
+    public static class recievedMessageViewHolder extends RecyclerView.ViewHolder{
+        private RecievedMessageContainerBinding binding;
+        public recievedMessageViewHolder(RecievedMessageContainerBinding binding){
+            super(binding.getRoot());
+            this.binding=binding;
+        }
+        private void setData(ChatMessage chatMessage,Bitmap recieverProfileImage){
+            binding.textMessage.setText(chatMessage.getMessage());
+            binding.textDateTime.setText(chatMessage.getDateTime());
+            binding.userProfile.setImageBitmap(recieverProfileImage);
+        }
     }
 }
--- app/src/main/java/com/example/instantchattingapp/models/ChatMessage.java
+++ app/src/main/java/com/example/instantchattingapp/models/ChatMessage.java
@@ -3,4 +3,40 @@ package com.example.instantchattingapp.models;
 public class ChatMessage {
     private String senderId,recieverId,message,dateTime;
 
+    public ChatMessage(String senderId, String recieverId, String message, String dateTime) {
+        this.senderId = senderId;
+        this.recieverId = recieverId;
+        this.message = message;
+        this.dateTime = dateTime;
+    }
+    public ChatMessage(){
+    }
+
+    public String getSenderId() {
+        return senderId;
+    }
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+
+    public String getRecieverId() {
+        return recieverId;
+    }
+    public void setRecieverId(String recieverId) {
+        this.recieverId = recieverId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
 }
